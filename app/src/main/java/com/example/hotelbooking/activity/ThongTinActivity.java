1e7cd9c200c0012181159ac13feb5f283ad5eb7b
16520723@gm.uit.edu.vn <mAnhtipro998@1>
2020-05-22 21:22:06 +0700

    Cập nhật lại phần giỏ hàng +Chỉnh class calendar +CHỉnh sửa logic phần tính tiền phòng +button tiếp tục chọn phòng

--- app/src/main/java/com/example/hotelbooking/activity/ThongTinActivity.java
+++ app/src/main/java/com/example/hotelbooking/activity/ThongTinActivity.java
@@ -1,16 +1,72 @@
 package com.example.hotelbooking.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hotelbooking.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ThongTinActivity extends AppCompatActivity {
+public class ThongTinActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Toolbar toolbarthongtin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_tin);
+        toolbarthongtin = (Toolbar) findViewById(R.id.toolbarthongtin);
+        ActionBar();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void ActionBar(){
+        setSupportActionBar(toolbarthongtin);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbarthongtin.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+
+        // Add a marker in Sydney and move the camera
+        LatLng tamtraquan = new LatLng(10.895553, 106.762126);
+        mMap.addMarker(new MarkerOptions().position(tamtraquan).title("Cafe Tâm Trà Quán").snippet("133 Đường số 4 Dĩ An Bình Dương").icon(BitmapDescriptorFactory.defaultMarker()));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(tamtraquan));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(tamtraquan).zoom(17).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 }
